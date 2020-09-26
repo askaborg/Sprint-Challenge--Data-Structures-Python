0875f30902d4f56109f3826171588f0cf412639f
askaborg <hienle@askaborg.com>
2020-09-25 18:53:50 -0700

    ring around

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,23 +1,22 @@
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.storage = []    # List for items
+        self.storage = []    # list for items
         self.index = 0
 
     def append(self, item):
-        # go to the end of the list
+        # add item to storage when there is capacity
         if len(self.storage) < self.capacity:
             self.storage.append(item)
 
-        # add the item to the list
+        # otherwise overwrite oldest
         else:
             self.storage[self.index] = item
             self.index += 1
-
-            # when capacity is reached overwrite the oldest
+            # ring around the storage
             if self.index == self.capacity:
                 self.index = 0
 
     def get(self):
-        # get items in stoarge
+        # get items in storage
         return [item for item in self.storage if item]
